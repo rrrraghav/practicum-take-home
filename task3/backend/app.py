cc52a52c8d2526223dd63a7405367127a01f6710
CamPlume1 <cam.k.plume@gmail.com>
2024-10-30 15:00:25 -0400

    Adding backend

--- task3/backend/app.py
+++ task3/backend/app.py
@@ -2,20 +2,21 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/groceries', methods=['GET'])
+@app.route('/account', methods=['GET'])
 def grocery_list():
-    # Hardcoded grocery list with item counts
-    groceries = {
-        "apples": 2,
-        "bananas": 2,
-        "milk": 2,
-        "bread": 1,
-        "eggs": 1,
-        "carrots": 1,
-        "chicken": 1
+    bank_account = {
+        "name": "Dr. Nilex Tietgione",
+        "balance": 35000,
+        "pending_charges" : [
+            12,
+            14,
+            -3,
+            400,
+            35
+        ]
     }
     
-    return jsonify(groceries)
+    return jsonify(bank_account)
 
 if __name__ == '__main__':
     app.run(debug=True)
