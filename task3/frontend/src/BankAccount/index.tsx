8d1596fbc58ad89117339ab25d405d694d231b2a
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-10-31 09:19:32 -0400

    task3

--- task3/frontend/src/BankAccount/index.tsx
+++ task3/frontend/src/BankAccount/index.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import {data} from "./data.ts"
+import { data } from "./data.ts"
 
 
 const BankAccount: React.FC = () => {
 
     const items = data
-    
-return (
-   <div>
-    <text>Bank Account Holder: {items["name"]}</text>
-    <text>Balance: ${items["balance"]}</text>
-    <text>Pending Charges: ${items["pending"]}</text>
-   </div> 
-)
+
+    return (
+        <div>
+            <div className="accountName">Bank Account Holder: {items["name"]}</div>
+            <div className="accountDetailsWrapper">
+                <div className="balanceCard">
+                    <div className="balanceTitle">Current Balance</div>
+                    <div className="balanceAmount">${items["balance"]}</div>
+                </div>
+                <div className="balanceCard">
+                    <div className="balanceTitle">Balance</div>
+                    <div className="balanceAmount">${items["balance"]}</div>
+                </div>
+            </div>
+            <text>Pending Charges: ${items["pending"]}</text>
+        </div>
+    )
 }
 
 export default BankAccount
