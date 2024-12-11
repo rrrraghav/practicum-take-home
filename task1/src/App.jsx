bcf681f33ad4745558717b447444b4b58e188a67
rrrraghav <raghavm0304@gmail.com>
2024-12-11 13:57:20 -0500

    task 1

--- task1/src/App.jsx
+++ task1/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="content">
-      <SongCard imagePath='album_cover.png' title="You Belong With Me"/>
+      <SongCard imagePath='album_cover.png' title="You Belong With Me" artist="Taylor Swift" album="Fearless" released="April, 20 2009"/>
     </div>
   )
 }
--- task1/src/SongCard/index.jsx
+++ task1/src/SongCard/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import './index.css';
 
-const SongCard  = ({imagePath, title}) => {
+const SongCard  = ({imagePath, title, artist, album, released}) => {
 
     return (
         <div className="cardWrapper">
-            <div>{title}</div>
+            <div className="title">{title}</div>
             <img src={imagePath} alt="Cover Not Found"  style={{ width: "150px", height: "150px", objectFit: "cover" }} />
+            <div className="track-info"><b>Artist:</b> {artist}</div>
+            <div className="track-info"><b>Album:</b> {album}</div>
+            <div className="track-info"><b>Released:</b> {released}</div>
             <button className="buttonPrimary">SELECT SONG</button>
         </div>
     )
